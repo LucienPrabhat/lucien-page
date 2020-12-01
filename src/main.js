d44dce067186cb795c51558682e303c3ef76faa1
LucienPrabhat <ww28xx@gmail.com>
2020-12-01 16:06:06 +0800

    debug: add logics in "aws-amplify/ui-vue" index to temporary issue of lib import error issue

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import Amplify from 'aws-amplify';
+
 // import '@aws-amplify/ui-vue';
+import { applyPolyfills, defineCustomElements } from '@aws-amplify/ui-components/loader';
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
+
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.config.isCustomElement = (tag) => tag.startsWith('amplify-');
+app.mount('#app');
