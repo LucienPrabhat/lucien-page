0cbe27d8d6a268f3f43748862f6a8ed125c0c1aa
LucienPrabhat <ww28xx@gmail.com>
2020-12-01 16:03:29 +0800

    debug: disable issue lib "aws-amplify/ui-vue" temporary

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import Amplify from 'aws-amplify';
-import '@aws-amplify/ui-vue';
+// import '@aws-amplify/ui-vue';
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
